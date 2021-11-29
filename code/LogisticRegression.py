792c25736a0e593200565d5c4e0ec3ee218ed03b
Baptman17 <baptiste.loutan@gmail.com>
2021-11-29 16:55:34 +0100

    Started logistic regression

---
+++ code/LogisticRegression.py
@@ -0,0 +1,30 @@
+from sklearn.metrics import precision_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from util import get_training_data, get_tfidf_vector, evaluate
+import pandas as pd
+
+print("Getting data...")
+df = get_training_data()
+df["difficulty"] = df["difficulty"].astype("category")
+
+print("Splitting data...")
+X = df['sentence']
+y = df['difficulty']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234, stratify=y)
+
+classifier = LogisticRegression()
+
+pipe = Pipeline([('vectorizer', get_tfidf_vector()),
+                 ('classifier', classifier)])
+
+print("Fitting the model...")
+pipe.fit(X_train, y_train)
+
+print("Predicting the values...")
+y_pred = pipe.predict(X_test)
+
+print("Evaluating the prediction...")
+evaluate(y_test, y_pred)
--- code/util.py
+++ code/util.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import spacy
+from sklearn.metrics import precision_score, recall_score, f1_score, confusion_matrix, accuracy_score
 from spacy.lang.fr.stop_words import STOP_WORDS
 from spacy.lang.fr import French
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+import string
 
 stop_words = spacy.lang.fr.stop_words.STOP_WORDS
-sp = spacy.load('fr_core_web_sm')
+sp = spacy.load('fr_core_news_sm')
+punctuations = string.punctuation
+
 
 def get_training_data():
     url = "https://raw.githubusercontent.com/Baptman17/DMML2021_Google/main/data/training_data.csv"
@@ -22,11 +27,22 @@ def spacy_tokenizer(sentence):
 
     # Lemmatize each token and convert each token into lowercase
     mytokens = [ word.lemma_.lower().strip() for word in mytokens ]
-    ## alternative way
-    # mytokens = [ word.lemma_.lower().strip() if word.lemma_ != "-PRON-" else word.lower_ for word in mytokens ]
 
     # Remove stop words and punctuation
     mytokens = [ word for word in mytokens if word not in stop_words and word not in punctuations ]
 
     # Return preprocessed list of tokens
     return mytokens
+
+
+def get_tfidf_vector():
+    return TfidfVectorizer(tokenizer=spacy_tokenizer)
+
+
+def evaluate(true, pred):
+    precision = precision_score(true, pred, average='micro')
+    recall = recall_score(true, pred, average='micro')
+    f1 = f1_score(true, pred, average='micro')
+    print(f"CONFUSION MATRIX:\n{confusion_matrix(true, pred)}")
+    print(f"ACCURACY SCORE:\n{accuracy_score(true, pred):.4f}")
+    print(f"CLASSIFICATION REPORT:\n\tPrecision: {precision:.4f}\n\tRecall: {recall:.4f}\n\tF1_Score: {f1:.4f}")
