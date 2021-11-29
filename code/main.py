078fc2390c29e6e65a2e2a2f7cc8bb0a4d876280
Baptman17 <baptiste.loutan@gmail.com>
2021-11-29 15:35:16 +0100

    add get to unlabelled data

--- code/main.py
+++ code/main.py
@@ -1,4 +1,7 @@
 from util import get_training_data
+import spacy
+from spacy import displacy
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 df = get_training_data()
 print(df.head(10))
--- code/util.py
+++ code/util.py
@@ -3,3 +3,7 @@ import pandas as pd
 def get_training_data():
     url = "https://raw.githubusercontent.com/Baptman17/DMML2021_Google/main/data/training_data.csv"
     return pd.read_csv(url, delimiter=",")
+
+def get_unlabelled_test_data():
+    url = "https://raw.githubusercontent.com/Baptman17/DMML2021_Google/main/data/unlabelled_test_data.csv"
+    return pd.read_csv(url, delimiter=",")
